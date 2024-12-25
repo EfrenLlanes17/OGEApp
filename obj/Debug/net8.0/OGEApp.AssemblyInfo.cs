a4879a0a4b9cae7671bdc413682973ea868181a6
EfrenLlanes17 <efrenllanes17@gmail.com>
2024-12-25 03:49:46 -0600

    Did UI Elements for Home, Activity, and half of Statistics

--- obj/Debug/net8.0/OGEApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/OGEApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7343d928be87f8672df0431d146e4882b3844809")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da632f5281c80f72f07a8e67d624e7977f5d58de")]
 [assembly: System.Reflection.AssemblyProductAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
