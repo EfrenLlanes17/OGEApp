febf0b9a26e9397e9f88470de71d6b3f5da5296e
EfrenLlanes17 <efrenllanes17@gmail.com>
2024-12-25 02:46:52 -0600

    removed side Nav Bar, Added top right navigation links to activity, statistcs, and map pages as well as location selection

--- obj/Debug/net8.0/OGEApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/OGEApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+830830ba831e9518f2c48924c3033f3b8fedff4b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7343d928be87f8672df0431d146e4882b3844809")]
 [assembly: System.Reflection.AssemblyProductAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
