9331dca42793bac92ead9feaa5fcad44de4f9f8b
EfrenLlanes17 <efrenllanes17@gmail.com>
2025-02-09 01:33:47 -0600

    Activity Page can access var with CSV contents inside

--- obj/Debug/net8.0/OGEApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/OGEApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8746c96a992baa9c79293f0d5d24348f7d410c02")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f723dff134572f1af3d911395078c9e3e1eb3e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
