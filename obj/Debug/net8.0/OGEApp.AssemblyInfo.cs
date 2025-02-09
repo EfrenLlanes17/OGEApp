a43e148ae3ba844fa69b5ff5e6981af1af377371
EfrenLlanes17 <efrenllanes17@gmail.com>
2025-02-09 11:11:46 -0600

    Activity Page can turn string data into 2D array and acess values in the array. But it does not do it by default

--- obj/Debug/net8.0/OGEApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/OGEApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f723dff134572f1af3d911395078c9e3e1eb3e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9331dca42793bac92ead9feaa5fcad44de4f9f8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
