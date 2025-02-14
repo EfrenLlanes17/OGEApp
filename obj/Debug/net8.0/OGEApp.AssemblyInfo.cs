895d864c2eb6c0b5acf953ec79caffd9e1bdaab2
EfrenLlanes17 <efrenllanes17@gmail.com>
2025-02-13 23:18:02 -0600

    upload works with mini sample but not large

--- obj/Debug/net8.0/OGEApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/OGEApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a04e4b53201d4f67670bdc0cf8e1747e6206e4d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7e7e5eba86fc0d70af224558d0bcae296f93cc7")]
 [assembly: System.Reflection.AssemblyProductAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
