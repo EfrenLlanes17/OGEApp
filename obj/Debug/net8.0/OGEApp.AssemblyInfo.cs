6f68a8642f1a4ae487de851933462d7560b2fec3
EfrenLlanes17 <efrenllanes17@gmail.com>
2025-03-25 10:22:28 -0500

    Can Modifiy Mainlayout location dropdown

--- obj/Debug/net8.0/OGEApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/OGEApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cc91384e8a34adfcf384b95693bbe615dd29149")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb306deed396c72abdf73efcb9ea854fd2ecb105")]
 [assembly: System.Reflection.AssemblyProductAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
