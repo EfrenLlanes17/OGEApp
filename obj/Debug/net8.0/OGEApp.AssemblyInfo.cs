59d909ed46b679f6e04cb50a5da97a2f2d608421
EfrenLlanes17 <efrenllanes17@gmail.com>
2025-03-31 10:55:46 -0500

    most active people almost done

--- obj/Debug/net8.0/OGEApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/OGEApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c0077b7e9a9a9d9b9bc726d8f18c2b30d36cb07")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0a77dc0048c40a427212baabefdf933e05c6008")]
 [assembly: System.Reflection.AssemblyProductAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
