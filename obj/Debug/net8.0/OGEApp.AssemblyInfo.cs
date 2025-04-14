3d795402b57ab5070a2535b9eb25468ea5284c53
EfrenLlanes17 <efrenllanes17@gmail.com>
2025-04-14 15:44:29 -0500

    substing for readers

--- obj/Debug/net8.0/OGEApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/OGEApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d773ce264ab2a61f35c78e4b030785cca320f95")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af6c7f7fcb1a391e594dafaec599578b595f41c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
