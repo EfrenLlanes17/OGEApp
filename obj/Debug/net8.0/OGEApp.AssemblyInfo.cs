624ed246602b3b4d319d8ffa5109c828a67b77b3
EfrenLlanes17 <efrenllanes17@gmail.com>
2025-04-14 23:20:25 -0500

    date in timeframe changes based on uploaded file

--- obj/Debug/net8.0/OGEApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/OGEApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d795402b57ab5070a2535b9eb25468ea5284c53")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b09ccb483be1b9f1eb1fb1b5fd9df0a722ecb0e")]
 [assembly: System.Reflection.AssemblyProductAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
