46910f18ca406587e631a78b15b7847965832cc1
EfrenLlanes17 <efrenllanes17@gmail.com>
2025-04-16 10:57:11 -0500

    finished Date Avg Filter on Avg Area Pop Graph

--- obj/Debug/net8.0/OGEApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/OGEApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+624ed246602b3b4d319d8ffa5109c828a67b77b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0efbcefaac8839b6350eaf7afb8c7e192c64e37")]
 [assembly: System.Reflection.AssemblyProductAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
