af9e401614a9a22bdd18d5990d8d07245cdb5a24
EfrenLlanes17 <efrenllanes17@gmail.com>
2025-04-17 15:24:20 -0500

    Readers work with issue ceck

--- obj/Debug/net8.0/OGEApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/OGEApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af566b403eff31657fc1304425f79db617be08d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+740efbd8c666e6e9f11488a63b90d30985b8cdab")]
 [assembly: System.Reflection.AssemblyProductAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
