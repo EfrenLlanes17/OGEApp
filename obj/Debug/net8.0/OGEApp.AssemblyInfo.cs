2bcc124ca81627156b32d724aac9f49133ae4a16
EfrenLlanes17 <efrenllanes17@gmail.com>
2025-04-17 22:30:21 -0500

    finished Ui of error display messages

--- obj/Debug/net8.0/OGEApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/OGEApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+740efbd8c666e6e9f11488a63b90d30985b8cdab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01076aa6c9d5590b5673c4835b49441f8a3f5d9a")]
 [assembly: System.Reflection.AssemblyProductAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
