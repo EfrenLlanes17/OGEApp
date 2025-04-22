38ccebfcf8db8e40adce0c86d660bb86d3643036
EfrenLlanes17 <efrenllanes17@gmail.com>
2025-04-22 10:33:00 -0500

    Click Correctly Passes data to full list page on pannel page

--- obj/Debug/net8.0/OGEApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/OGEApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49a3881c1ebd1398d20396fa879049ee2448f287")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdaf8014d3c433c73e7fc6c19758ad27dc5d0f56")]
 [assembly: System.Reflection.AssemblyProductAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
