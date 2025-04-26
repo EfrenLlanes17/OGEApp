1b0f4b88fb75f684b84addf0ee505296222e2ad5
EfrenLlanes17 <efrenllanes17@gmail.com>
2025-04-25 23:12:37 -0500

    Changed 1st Graph to avg scans at times

--- obj/Debug/net8.0/OGEApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/OGEApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+977107649302bfaebcd76decd334ff875bf9fe67")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c0ba795b0b571c3efe867e2ddc93998ff604060")]
 [assembly: System.Reflection.AssemblyProductAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
