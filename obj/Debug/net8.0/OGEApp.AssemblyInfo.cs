572ee655b8fa4b622427b293c1d12df10a23ab35
EfrenLlanes17 <efrenllanes17@gmail.com>
2025-04-25 23:37:33 -0500

    Changed some UI and Updating of filters to work with scan times. And made scan times and scan activations avg

--- obj/Debug/net8.0/OGEApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/OGEApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c0ba795b0b571c3efe867e2ddc93998ff604060")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b0f4b88fb75f684b84addf0ee505296222e2ad5")]
 [assembly: System.Reflection.AssemblyProductAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
