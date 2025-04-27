3f8be9d008bb0b8ffb36019d43628803d85bdd61
EfrenLlanes17 <efrenllanes17@gmail.com>
2025-04-26 23:44:20 -0500

    Made panels On by default in map and switched pannel Icon

--- obj/Debug/net8.0/OGEApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/OGEApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b0f4b88fb75f684b84addf0ee505296222e2ad5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2485cd6cdcac0278a3b6843a5b5251153bb0235")]
 [assembly: System.Reflection.AssemblyProductAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
