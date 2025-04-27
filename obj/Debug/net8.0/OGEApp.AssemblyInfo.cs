d7da6f6cadbf42e135f1e94a0f1fa1058ed4a5b0
EfrenLlanes17 <efrenllanes17@gmail.com>
2025-04-27 10:12:31 -0500

    Started Heat Map

--- obj/Debug/net8.0/OGEApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/OGEApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7eb0d0f1c014e211696d1083a8a433edec1c85f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+865d206141e601749518a420fee32c241c202c18")]
 [assembly: System.Reflection.AssemblyProductAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
