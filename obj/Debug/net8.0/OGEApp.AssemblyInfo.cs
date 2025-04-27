243e20c6098b6f08df09a8e738366c5e7c356258
EfrenLlanes17 <efrenllanes17@gmail.com>
2025-04-27 00:57:48 -0500

    Ui and some functinality on panel popup

--- obj/Debug/net8.0/OGEApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/OGEApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f8be9d008bb0b8ffb36019d43628803d85bdd61")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1be16e9e9f5bef8a35a06422b8b7ca86a49443a")]
 [assembly: System.Reflection.AssemblyProductAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
