d4fdb3e5dbe10b51cbcf874c5e8c88e3d6bbf85c
EfrenLlanes17 <efrenllanes17@gmail.com>
2025-04-27 02:49:08 -0500

    All info in reader popup is correct

--- obj/Debug/net8.0/OGEApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/OGEApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1be16e9e9f5bef8a35a06422b8b7ca86a49443a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7eb0d0f1c014e211696d1083a8a433edec1c85f")]
 [assembly: System.Reflection.AssemblyProductAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
