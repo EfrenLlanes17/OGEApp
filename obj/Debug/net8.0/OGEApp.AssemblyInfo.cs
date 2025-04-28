c565d644b62e909549b6f46fd787e6ab243fdae4
EfrenLlanes17 <efrenllanes17@gmail.com>
2025-04-28 16:57:05 -0500

    PDF has images at top

--- obj/Debug/net8.0/OGEApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/OGEApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+771ad3445c05fdd9b0a6fb6154f28f6feb0b1178")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6511238de4de3b440a57fc7e43c28a5936a47d25")]
 [assembly: System.Reflection.AssemblyProductAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
