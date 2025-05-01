31f713940ccca93d02a440326eb64885c273b10b
EfrenLlanes17 <efrenllanes17@gmail.com>
2025-05-01 01:12:36 -0500

    Changed Times to CTE

--- obj/Debug/net8.0/OGEApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/OGEApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d909f0dbf8f30897c2df50d3ddde319bdfd0cefe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+412df5cffe0dca9d13dbe0a1cc83a2fcc0e0eb4c")]
 [assembly: System.Reflection.AssemblyProductAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
