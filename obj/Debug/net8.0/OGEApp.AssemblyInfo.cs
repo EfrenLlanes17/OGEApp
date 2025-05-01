1a38128664644df642664324c069baf5b497f03d
EfrenLlanes17 <efrenllanes17@gmail.com>
2025-05-01 17:01:38 -0500

    Works with multiple Locations

--- obj/Debug/net8.0/OGEApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/OGEApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+675cda8967a6864059f3bbf93482131cd099ae7f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f41f93fd63a73c59651677fb42e61022363e3fbf")]
 [assembly: System.Reflection.AssemblyProductAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
