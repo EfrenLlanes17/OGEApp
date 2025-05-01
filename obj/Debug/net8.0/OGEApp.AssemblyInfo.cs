ec699d53181c9bf298127ca1a077c49cc5200ad4
EfrenLlanes17 <efrenllanes17@gmail.com>
2025-05-01 01:57:00 -0500

    Map maintence function is much faster

--- obj/Debug/net8.0/OGEApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/OGEApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31f713940ccca93d02a440326eb64885c273b10b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b530910bfef0251ee5edd914c2c328cd3ce8c2c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
