1f36d769bd0774c7229c02f1579aec47224f58c5
EfrenLlanes17 <efrenllanes17@gmail.com>
2025-05-06 10:32:10 -0500

    Issue Full List Ui work

--- obj/Debug/net8.0/OGEApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/OGEApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c19c5076cafb94beb4c0c4b775b9b3afee1e177")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2d7b614f35d73b3700fadae97773238ca2d37f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
