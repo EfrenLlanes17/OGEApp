658ff57dc884a603c1a7db3fcd1035a6ff257ea7
EfrenLlanes17 <efrenllanes17@gmail.com>
2025-05-06 11:02:52 -0500

    Finished Days of the week graph pop up

--- obj/Debug/net8.0/OGEApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/OGEApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2d7b614f35d73b3700fadae97773238ca2d37f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76b05a256fbee9dd43149da59532d21a35a0b5de")]
 [assembly: System.Reflection.AssemblyProductAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OGEApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
